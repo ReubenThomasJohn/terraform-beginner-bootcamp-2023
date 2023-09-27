ffb977cfb740d267042774f9239aed0c3cdae5f9
Reuben Thomas <72449974+ReubenThomasJohn@users.noreply.github.com>
2023-09-28 01:39:30 +0530

    #12 install tf provider, create s3 bucket (#13)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
   }
 }
 
@@ -12,9 +16,17 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  lower = true
+  upper = false
+  length           = 32
   special          = false
 }
+
+resource "aws_s3_bucket" "example" {
+  # Bucket naming rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
+} 
 
 output "random_bucket_name" {
     value = random_string.bucket_name.result
