86e2848387046472366ca0ffe6f33a7e94cd6b02
Reuben Thomas <72449974+ReubenThomasJohn@users.noreply.github.com>
2023-10-07 19:08:29 +0530

    #26 refactor s3 bucket into terrahouse_aws module (#27)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -26,3 +26,14 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "website_bucket" {
+  # Bucket naming rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = var.bucket_name
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+} 
+
