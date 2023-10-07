3177646b7a7518d9486de97c895616f4a35a1684
Reuben Thomas <72449974+ReubenThomasJohn@users.noreply.github.com>
2023-10-07 20:39:23 +0530

    #28 enabled static website hosting, uploaded index and error file via terraform (#29)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -16,3 +16,21 @@ variable "bucket_name" {
   #   error_message = "Invalid bucket name. The name must consist of lowercase letters, numbers, hyphens, and periods, and must start and end with a lowercase letter or number."
   # }
 }
+
+variable "index_html_filepath" {
+  type        = string
+  description = "Filepath to the index HTML file"
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The specified index HTML file does not exist."
+  }
+}
+
+variable "error_html_filepath" {
+  type        = string
+  description = "Filepath to the error HTML file"
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The specified index HTML file does not exist."
+  }
+}
