7ee1556edc1b648e8cd61d7c1410a3fef5ed5a45
Reuben Thomas <reubenthomas.john2017@vitstudent.ac.in>
2023-10-07 15:08:40 +0000

    #28 enabled static website hosting, uploaded index and error file via terraform

--- output.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
-}
---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "bucket_name" {
+  description = "Bucket name for our static website hosting"
+  value = module.terrahouse_aws.bucket_name
+}
+
+output "s3_website_endpoint" {   
+  description = "S3 static webiste hosting endpoint"
+    value = module.terrahouse_aws.website_endpoint
+}
