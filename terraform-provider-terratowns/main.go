aca6e4e26181caad68bfbacc345e39cc90e5e17e
Reuben Thomas <72449974+ReubenThomasJohn@users.noreply.github.com>
2023-10-12 19:37:15 +0530

    40 terratowns provider (#41)
    * started writing provider
    
    * #40 wip setting up the skeleton for creating a custom tf provider

---
+++ terraform-provider-terratowns/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: Provider,
+	})
+	fmt.Println("Hello, world!")	
+}
+
+// In golang, a titlecase function will get exported
+func Provider() *schema.Provider {
+	var p *schema.Provider
+	p = &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+
+		},
+		Schema: map[string]*schema.Schema{
+			"endpoint": {
+				Type: schema.TypeString,
+				Required: true,
+				Description: "The endpoint for the external service",
+			},
+			"token": {
+				Type: schema.TypeString,
+				Required: true,
+				Sensitive: true,
+				Description: "Bearer token for auth",
+			},
+			"user_uuid": {
+				Type: schema.TypeString,
+				Required: true,
+				Description: "UUID for configuration",
+				// ValidateFunc: validateUUID,
+			},
+		},
+	}
+	// p.ConfigureContextFunc = providerConfigure(p)
+	return p
+}
+
+// func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+// 	log.Print("validateUUID:start")
+// 	value := v.(string)
+// 	if _, err := uuid.Parse(value); err != nil {
+// 		errors = append(errors, fmt.Errorf("invalid UUID format"))
+// 	}
+// 	log.Print("validateUUID:end")
+// 	return
+// }
+
+// func providerConfigure(p *schema.Provider) schema.ConfigureContextFunc {
+// 	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics ) {
+// 		log.Print("providerConfigure:start")
+// 		config := Config{
+// 			Endpoint: d.Get("endpoint").(string),
+// 			Token: d.Get("token").(string),
+// 			UserUuid: d.Get("user_uuid").(string),
+// 		}
+// 		log.Print("providerConfigure:end")
+// 		return &config, nil
+// 	}
+// }
