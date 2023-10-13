6578195c55eaac6d5a3403cf83a7242fb8a33281
Reuben Thomas <reubenthomas.john2017@vitstudent.ac.in>
2023-10-13 21:01:56 +0000

    #5tf cloud used, multi refactor needs to be done

--- main.tf
+++ main.tf
@@ -38,6 +38,26 @@ link you to some of my favourite songs
   DESCRIPTION
   domain_name = module.terrahouse_aws.cloudfront_url
   # domain_name = "3fdq3gzzz.cloudfront.net"
-  town = "melomaniac mansion"
+  town = "gamers_grotto"
   content_version = 1
 }
+
+# module "terrahouse_aws" {
+#     source = "./modules/terrahouse_aws"
+#     user_uuid = var.teacherseat_user_uuid
+#     index_html_filepath = var.index_html_filepath
+#     error_html_filepath = var.error_html_filepath
+#     content_version = var.content_version
+#     assets_path = var.assets_path
+# }
+
+# resource "terratowns_home" "home" {
+#   name = "Making Tandoori Chicken"
+#   description = <<DESCRIPTION
+# Tandoori chicken is amazing. 
+#   DESCRIPTION
+#   domain_name = module.terrahouse_aws.cloudfront_url
+#   # domain_name = "3fdq3gzzz.cloudfront.net"
+#   town = "melomaniac_mansion"
+#   content_version = 1
+# }
